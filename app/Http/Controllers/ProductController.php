c45427df03b68693e233fb8e9364c0f56e29f641
ramiro-c <rami992009@gmail.com>
2020-03-24 19:53:43 -0300

    Funcion para traer los productos por el id de la categoria

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Product;
-use Illuminate\Http\Request;
-use Response;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
-    
-    public function show($product_id)
+    public function show()
     {
         //Mostrar datos del producto segun el ID enviado
         $product = Product::all();
@@ -18,15 +17,36 @@ class ProductController extends Controller
         $imagesLeft = collect();
         $imagesRight = collect();
         foreach ($images as $key => $image) {
-            if($key%2 === 0){
+            if ($key % 2 === 0) {
                 $imagesLeft->push($image);
             } else {
                 $imagesRight->push($image);
             }
         }
+        $imagesTotal = array();
         $imagesTotal->imagesLeft = $imagesLeft;
         $imagesTotal->imagesRight = $imagesRight;
         return Response::json($imagesTotal);
     }
 
+    public function productsByCategoryId($category_id)
+    {
+        /* FALTA TRAER LAS IMAGENES */
+
+        $products = Product::where('category_id', $category_id)->get();
+        Log::info('Obtengo los productos de cierta categoria');
+
+        if ($products) {
+            $response = array();
+            foreach ($products as $product) {
+                array_push($response, $product);
+            }
+            Log::info('Enviamos los productos correctamente');
+            return Response::json($response, 200);
+        }
+
+        Log::error('No se pudo obtener los categorias');
+        $response = array("response" => 'No se encontraron resultados');
+        return Response::json($response, 404);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware(['cors'])->group(function () {
     //Rutas para logout
     Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
     Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
-    
+
     //Rutas para login
     Route::post('login', 'AuthenticateController@authenticate')->name('login');
 
@@ -38,31 +38,33 @@ Route::middleware(['cors'])->namespace('Admin')->group(function () {
     Route::resource('categories', 'CategoryController');
     Route::get('categories-featured', 'CategoryController@categoriesFeatured');
     //Products
-    Route::get('/admin/products', 'ProductController@index');            //Listado
+    Route::get('/admin/products', 'ProductController@index');                           //Listado
 
-    Route::get('/admin/products-featured', 'ProductController@productsFeatured');      //Listado products featured
-    Route::get('/admin/last-products', 'ProductController@lastProducts');      //Listado ultimos 15 products
-    Route::get('/admin/products/create', 'ProductController@create');   //Crear
-    Route::get('/admin/products/{product_id}/edit', 'ProductController@edit');      //Editar
-    Route::post('/admin/products/{product_id}/edit', 'ProductController@update');   //Editar
-    Route::get('/admin/products/{product_id}/inactive', 'ProductController@inactive');   //Editar
-    Route::delete('/admin/products/{product_id}', 'ProductController@destroy');   //Eliminar
-    Route::post('/admin/products', 'ProductController@store');           //Registrar
-    
+    Route::get('/admin/products-featured', 'ProductController@productsFeatured');       //Listado products featured
+    Route::get('/admin/last-products', 'ProductController@lastProducts');               //Listado ultimos 15 products
+    Route::get('/admin/products/create', 'ProductController@create');                   //Crear
+    Route::get('/admin/products/{product_id}/edit', 'ProductController@edit');          //Editar
+    Route::post('/admin/products/{product_id}/edit', 'ProductController@update');       //Editar
+    Route::get('/admin/products/{product_id}/inactive', 'ProductController@inactive');  //Editar
+    Route::delete('/admin/products/{product_id}', 'ProductController@destroy');         //Eliminar
+    Route::post('/admin/products', 'ProductController@store');                          //Registrar
+
     //IMAGES
-    Route::get('/products/images', 'ProductImageController@index');            //Listado
-    Route::post('/products/{product_id}/images', 'ProductImageController@store');           //Registrar
-    Route::delete('/products/{product_id}/images', 'ProductImageController@destroy');       //Eliminar
+    Route::get('/products/images', 'ProductImageController@index');                     //Listado
+    Route::post('/products/{product_id}/images', 'ProductImageController@store');       //Registrar
+    Route::delete('/products/{product_id}/images', 'ProductImageController@destroy');   //Eliminar
     Route::post('/products/{product_id}/images/select/{image_id}', 'ProductImageController@select');
 
     //Route::get('/categories/{category}', 'CategoryController@show');
 });
 
 //Route::middleware(['cors','jwt.auth'])->group(function(){
-Route::middleware(['cors'])->group(function(){
+Route::middleware(['cors'])->group(function () {
+
     Route::get('/products/{product_id}', 'ProductController@show');
     //Search
     Route::get('/search', 'SearchController@show');
+    Route::get('/products/{category_id}', 'ProductController@productsByCategoryId');
 
     //CartDetails
     Route::post('/cart', 'CartDetailController@store');
