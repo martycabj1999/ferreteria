b7b0db8bf383d5bb3629d3f981119de81846d17d
ramiro-c <rami992009@gmail.com>
2020-03-25 19:08:48 -0300

    Funcion nueva para traer los productos segun la id de la categoria

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,50 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Category;
+use App\ProductImage;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
-    public function show()
-    {
-        //Mostrar datos del producto segun el ID enviado
-        $product = Product::all();
-        $images = $product->images;
+  public function show()
+  {
+    //Mostrar datos del producto segun el ID enviado
+    $product = Product::all();
+    $images = $product->images;
 
-        $imagesLeft = collect();
-        $imagesRight = collect();
-        foreach ($images as $key => $image) {
-            if ($key % 2 === 0) {
-                $imagesLeft->push($image);
-            } else {
-                $imagesRight->push($image);
-            }
-        }
-        $imagesTotal = array();
-        $imagesTotal->imagesLeft = $imagesLeft;
-        $imagesTotal->imagesRight = $imagesRight;
-        return Response::json($imagesTotal);
+    $imagesLeft = collect();
+    $imagesRight = collect();
+    foreach ($images as $key => $image) {
+      if ($key % 2 === 0) {
+        $imagesLeft->push($image);
+      } else {
+        $imagesRight->push($image);
+      }
+    }
+    $imagesTotal = array();
+    $imagesTotal->imagesLeft = $imagesLeft;
+    $imagesTotal->imagesRight = $imagesRight;
+    return Response::json($imagesTotal);
+  }
+
+  public function productsByCategoryId($category_id)
+  {
+    $products = Product::where('category_id', $category_id)->get();
+    Log::info('Obtengo los productos de cierta categoria');
+
+    if ($products) {
+      $response = array();
+      foreach ($products as $product) {
+        $category = Category::find($product['category_id']);
+        $images = ProductImage::where('product_id', $product['id'])->get();
+        $product['category'] = $category;
+        $product['images'] = $images;
+        array_push($response, $product);
+      }
+      Log::info('Enviamos los productos correctamente');
+      return Response::json($response, 200);
     }
 
-    public function productsByCategoryId($category_id)
-    {
-        /* FALTA TRAER LAS IMAGENES */
-
-        $products = Product::where('category_id', $category_id)->get();
-        Log::info('Obtengo los productos de cierta categoria');
-
-        if ($products) {
-            $response = array();
-            foreach ($products as $product) {
-                array_push($response, $product);
-            }
-            Log::info('Enviamos los productos correctamente');
-            return Response::json($response, 200);
-        }
-
-        Log::error('No se pudo obtener los categorias');
-        $response = array("response" => 'No se encontraron resultados');
-        return Response::json($response, 404);
-    }
+    Log::error('No se pudo obtener los categorias');
+    $response = array("response" => 'No se encontraron resultados');
+    return Response::json($response, 404);
+  }
 }
