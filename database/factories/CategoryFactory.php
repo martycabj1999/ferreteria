82551d9f7635a39071646fca53d7f314a6068e4b
Martin <martin@youmarket.com.ar>
2020-03-30 23:15:26 -0300

    images factories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
         'name' => ucfirst($faker->word),
+        'image' => "https://picsum.photos/id/".$faker->numberBetween(1,1000)."/640/480",
         'description' => $faker->sentence(10),
         'state_id' => 1
     ];
--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\ProductImage::class, function (Faker $faker) {
     return [
-        'image' => $faker->imageUrl(250,250),
+        'image' => "https://picsum.photos/id/".$faker->numberBetween(1,1000)."/640/480",
         'product_id' => $faker->numberBetween(1,100)
     ];
 });
